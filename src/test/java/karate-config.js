f612af7c065f9f734e4cf8dd7da24b3067b3694e
surbhi <surbhi.kalra@gfk.com>
2019-01-25 16:23:22 +0100

    Added the karate-config.js

---
+++ src/test/java/karate-config.js
@@ -0,0 +1,10 @@
+function()
+{
+    var config = {
+
+        gitUrl: 'https://api.github.com/users/octocat',
+        breweryUrl:'https://sandbox-api.brewerydb.com/v2/menu/categories/?key=8cabbe48d7506d7076c041f8ada767c2',
+};
+
+    return config;
+}
